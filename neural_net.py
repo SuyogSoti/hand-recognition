2443954d7b87cc24b63491b6225730212cfbc5fb
AveryAnderson <avan4780@colorado.edu>
2018-02-24 17:35:30 -0700

    skeleton

--- neural_net.py
+++ neural_net.py
@@ -1,8 +1,14 @@
 from sklearn.neural_network import MLPClassifier
+import numpy as np
 
 
 def main():
+    x = np.array([[0, 0, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0], [1, 0, 0, 0]])
+    print(x)
+    y = np.array([0, 1, 1, 1])
     ml = MLPClassifier()
+    ml.fit(x, y)
+    print(ml.predict([[0,0,1,0]]))
 
 
 if __name__ == '__main__':
