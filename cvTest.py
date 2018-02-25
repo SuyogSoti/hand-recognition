7546d5f34914cce319ac1192430fd4c4c9ca757a
Suyog Soti <suyog.soti@gmail.com>
2018-02-24 17:37:03 -0700

    image works now

--- cvTest.py
+++ cvTest.py
@@ -3,22 +3,25 @@
 import numpy as np
 import cv2
 
-x1, x2, y1, y2 = 100, 100, 300, 300
+x1, y1, x2, y2 = 50, 100, 250, 300
 
 cap = cv2.VideoCapture(0)
+
+cap.set(3, 800)
+cap.set(4, 600)
 
 while(True):
     #capture frame-by-frame
     ret, image = cap.read()
-
+    image = cv2.flip(image, 1)   
     #Operate on the frame
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.rectangle(image, (x1, y1), (x2,y2),(0,255,0),2)
+#    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
     
     subIm = image[y1:y2, x1:x2]
 
     #display the result
-    cv2.imshow('frame',image)
+    cv2.imshow('frame', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
